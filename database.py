d4a6c1d54fd9990d593e9c127e4809d1cb79ceb7
lllrikitikilll <lllrikitikilll@yandex.ru>
2023-08-28 21:09:23 +0300

    db decorate

--- database.py
+++ database.py
@@ -2,3 +2,14 @@ import sqlite3
 from datetime import datetime
 
 from telebot.types import Message, CallbackQuery
+
+def db(func):
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('perfume.db')
+        cursor = conn.cursor()
+        data = func(cursor=cursor, *args, **kwargs)
+        conn.commit()
+        conn.close()
+        return data
+
+    return wrapper
